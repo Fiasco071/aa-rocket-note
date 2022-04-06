cbf98308ef01bd7028882aced5dc14af3b918bd5
Steve Choi <stevech071@gmail.com>
2022-04-06 15:19:31 -0700

    notebook CRUD completed and tested

--- frontend/src/components/NotesNav/index.js
+++ frontend/src/components/NotesNav/index.js
@@ -2,29 +2,43 @@
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDownWideShort, faNoteSticky, faFilter, faBook } from '@fortawesome/free-solid-svg-icons';
-import { useHistory,useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 
 const NotesNav = () => {
     const history = useHistory();
     const { noteBookId } = useParams();
     const notesObj = useSelector(state => state.notes.entries);
-    const notebookObj = useSelector(state => state.notebooks.notebooks);
-    
-    // let notes;
-    // if (noteBookId) {
-    //     notes = Object.values(notesObj).filter(note => +noteBookId === note.noteBookId);
-    // } else {
-    //     notes = Object.values(notesObj);
-    // }
+    const notebooks = useSelector(state => state.notebooks.notebooks);
+    const [notebook, setNotebook] = useState('All')
+
+    let notes;
+    if (noteBookId) {
+        notes = Object.values(notesObj).filter(note => +noteBookId === note.noteBookId);
+        // setNotebook(Object.values(notesObj)[noteBookId])
+    } else {
+        notes = Object.values(notesObj);
+    }
+
+    console.log()
 
     return (
         <div className="notes-nav-bar">
             <div className="notes-nav-control-box">
                 <div className="notes-nav-control-box-head">
-                    <FontAwesomeIcon icon={faNoteSticky} />
-                    <p className='notes-nav-text'>{notebookObj[noteBookId]?.name}</p>
+                    <FontAwesomeIcon icon={faBook} />
+                    <select
+                        className='notebook-nav-select'
+                        value={notebook}
+                        onChange={(e) => (setNotebook(e.target.value))}
+                    >
+                        <option value='All'>All</option>
+                        {Object.values(notebooks).map(notebook => (
+                            <option key={notebook.id} value={notebook.id}>{notebook.name}</option>
+                        ))}
+                    </select>
                 </div>
                 <div className="notes-nav-control-box-buttons-box">
                     <div>
@@ -36,7 +50,7 @@ const NotesNav = () => {
                     </div>
                 </div>
             </div>
-            {Object.values(notesObj).map((note) => (
+            {notes.map((note) => (
                 <div
                     className="note"
                     key={note.id}
